e27261d0b780527e85562588aa3390afaab51441
Luísa <lumendes2001@gmail.com>
2025-06-02 21:34:31 -0300

    removing default nest files: app controller and app service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { PrismaModule } from './prisma.module';
 import { ReviewsModule } from './reviews/reviews.module';
@@ -10,7 +8,6 @@ import { AiService } from './ai/ai.service';
 
 @Module({
   imports: [BooksModule, PrismaModule, ReviewsModule, OrdersModule, CartModule], // aqui é onde vão ser feitas as importações de novos módulos
-  controllers: [AppController],
-  providers: [AppService, AiService],
+  providers: [AiService],
 })
 export class AppModule {}
